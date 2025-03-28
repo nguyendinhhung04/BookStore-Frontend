8e704b7a2762eccc1773d9672e253d8826b14eec
nguyendinhhung04 <dinhhung5a6@gmail.com>
2025-03-28 22:09:56 +0700

    Add user table in User.js to view user
    -Table scale the row with the data receive from backend

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,37 @@
+import {useState,useEffect} from "react";
+import axios from "axios";
+import {Table} from "react-bootstrap";
+
+
 export function User() {
+
+    const [userList, setUserList] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/admin/user/view")
+            .then(response => setUserList(response.data))
+            .catch(error => console.error("Error fetching data:", error));
+    }, []);
+
     return (
         <>
-            <table>
-                <thead>jhhsdsqdqdqd</thead>
+            <Table className="table table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                </tr>
+                </thead>
+
                 <tbody>
-                    <p>hung </p>
+                    {userList.map(user => (
+                        <tr>
+                            <td>{user.id}</td>
+                            <td>{user.fullname}</td>
+                        </tr>
+                    ))}
                 </tbody>
-            </table>
+            </Table>
         </>
     )
 }
