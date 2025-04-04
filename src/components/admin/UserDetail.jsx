fa26bd5efc49b8ca992d42c9ce22cf468786c733
nguyendinhhung04 <dinhhung5a6@gmail.com>
2025-04-04 10:42:06 +0700

    add react-alert-confirm module, add delete button

--- src/components/admin/UserDetail.jsx
+++ src/components/admin/UserDetail.jsx
@@ -1,14 +1,26 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState,useRef} from "react";
 import { Container, Row, Col, Form, Button, Image } from "react-bootstrap";
-import {useRef } from "react";
-import { useParams } from "react-router-dom";
+
+import { useParams,useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
+import AlertConfirm from "react-alert-confirm";
+import "react-alert-confirm/lib/style.css";
+
 export function UserDetail() {
     const [enabledModify, setEnabledModify] = React.useState(false);
-    console.log(enabledModify);
+    const [Alertvisible, setAlertVisible] = useState(false);
     const params = useParams();
+    const discardData = useRef(null);
+    const navigate = useNavigate();
+
+    AlertConfirm.config({
+        maskClosable: true,
+        closeAfter: () => {
+            console.log("close");
+        },
+    });
 
     const [user,setUser] = React.useState( {
         id : params.userId,
@@ -23,24 +35,53 @@ export function UserDetail() {
 
     const handleChanged = (event) => {
         setUser({...user, [event.target.name]: event.target.value});
+
     }
 
     const handleSubmited = (event) => {
         axios.post("http://localhost:8080/user/create", user)
-            .then(function (response) {console.log(response)})
-            .catch(error => {console.log(error)})
+            .then(function (response) {discardData.current = user;setEnabledModify(false);})
+            .catch(error => {console.log(error)});
+
+    }
+
+    const handleDiscarded = (event) => {
+        setUser(discardData.current);
+        setEnabledModify(false);
+    }
+
+    const handleDelete = (event) => {
+        axios.post(`http://localhost:8080/admin/user/delete/${user.id}`)
+            .then(response => {navigate("/admin/user/view/")})
+            .catch(error => {console.log(error)});
     }
 
     useEffect(() => {
         axios.get(`http://localhost:8080/admin/user/detail/${params.userId}`)
-            .then(response => setUser(response.data))
+            .then(response => { setUser(response.data); discardData.current = response.data; })
             .catch(error => console.error("Error fetching data:", error));
     }, []);
 
 
 
+
     return (
         <Container className="mt-4 p-4 border rounded shadow-lg bg-white" style={{ maxWidth: "600px" }}>
+
+            <AlertConfirm
+                maskClosable
+                title="Do you Want to delete these items?"
+                desc="Some descriptions"
+                visible={Alertvisible}
+                okText="Yes"
+                onOk={() => {
+                    setAlertVisible(false);
+                    handleDelete();
+                }}
+                cancelText="No"
+                onCancel={() => setAlertVisible(false)}
+            />
+
             <Row className="text-center mb-3">
                 <Col>
                     <Image
@@ -125,11 +166,13 @@ export function UserDetail() {
 
 
                 <div className="text-center" style={{ display: enabledModify === true ? "block" : "none" }}>
-                    <Button variant="primary" onClick={ ()=> {setEnabledModify(false); handleSubmited(); }} >Save</Button>
+                    <Button className="btn-secondary me-2" onClick={handleDiscarded}>Discard</Button>
+                    <Button className="btn-primary me-2" variant="primary" onClick={ ()=> { handleSubmited(); }} >Save</Button>
                 </div>
                 <div className="text-end" style={{ display: enabledModify === false ? "block" : "none" }}>
+
                     <Button variant="primary" className="btn-warning me-2" onClick={ ()=> {setEnabledModify(true)} }>Modify</Button>
-                    <Button variant="primary" className="btn-danger me-2">Delete</Button>
+                    <Button variant="primary" className="btn-danger me-2" onClick={()=>{setAlertVisible(true)}}>Delete</Button>
                 </div>
 
             </Form>
